cdf9e1d4b137b30cdfb64d8b121aecbfa3a93ce0
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-04-18 14:56:16 +0200

    Added functionality to range-slider

--- javascript.js
+++ javascript.js
@@ -4,10 +4,14 @@ let numOfBoxes=gridSize*gridSize;
 const slider = document.getElementById('slider');
 const sliderValue = document.getElementById('slider-value');
 
+window.onload = function() {
+    createGrid();
+};
 
 slider.addEventListener('input', function () {
     const value = slider.value;
-    sliderValue.textContent = value;
+    let sliderValueText=`GridSize: ${value}x${value}`
+    sliderValue.textContent = sliderValueText;
     gridSize = slider.value;
     clearGrid();
     createGrid();
